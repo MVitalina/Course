c164683f30b9d56d56894abe0dcde05be6027e31
Мірошник Віталіна <reckless.vita@gmail.com>
2018-05-28 02:25:19 +0300

    visible book info on click

--- src/sample/LibraryController.java
+++ src/sample/LibraryController.java
@@ -2,9 +2,13 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.sql.SQLException;
@@ -14,6 +18,10 @@ import java.util.ResourceBundle;
 public class LibraryController implements Initializable {
     @FXML
     private ListView <String> listView;
+    @FXML
+    private Pane pane;
+    @FXML
+    private Label titleL, authorL, isbnL, avL;
 
     private DBModel model = new DBModel();
 
@@ -30,13 +38,35 @@ public class LibraryController implements Initializable {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                pane.setVisible(true);
+                try {
+                    Book book = model.getBookList().get(indexOfBook());
+                    titleL.setText(book.title);
+                    authorL.setText(book.author);
+                    isbnL.setText(book.ISBN);
+                    if (book.availability.equals("not in use")) {
+                        avL.setText("Наявна");
+                    } else {
+                        avL.setText("Наразі книга на руках");
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
-    public LinkedList<String> getBookStringList(LinkedList<Book> books) {
-        LinkedList<String> stringList = new LinkedList<String>();
-        for (Book b: books ) {
-            stringList.add("\"" + b.title + "\"" + b.author);
+    public int indexOfBook() {
+        for (String s: itemList ) {
+            if (s.equals(listView.getSelectionModel().getSelectedItem())){
+                return itemList.indexOf(s);
+            }
         }
-        return stringList;
+        return -1;
     }
 }
